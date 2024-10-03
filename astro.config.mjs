6ff2c0bfd7db181e29b4250cc07d3628a8cf2579
runas24 <runnnasss4@gmail.com>
2024-10-03 18:47:56 +0500

    6

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  site: "https://sozda-site.ru", // Указываем основной домен сайта
+  base: "/", // У вас нет подкаталогов, поэтому базовый путь корневой
+  trailingSlash: config.site.trailing_slash ? "always" : "never", // Это берётся из config.json
   image: {
     service: squooshImageService(),
   },
